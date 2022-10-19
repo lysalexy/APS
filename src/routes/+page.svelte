184918f4e0c330de3958d31b5d41ebede21be7c4
LysenkoAleksandra <orange260302@gmail.com>
2022-10-19 16:03:16 +0300

    On branch main  Initial commit Changes to be committed: 	new file:   .eslintignore 	new file:   .eslintrc.cjs 	new file:   .gitignore 	new file:   .npmrc 	new file:   .prettierignore 	new file:   .prettierrc 	new file:   README.md 	new file:   jsconfig.json 	new file:   package-lock.json 	new file:   package.json 	new file:   playwright.config.js 	new file:   src/app.d.ts 	new file:   src/app.html 	new file:   src/lib/images/github.svg 	new file:   src/lib/images/svelte-logo.svg 	new file:   src/lib/images/svelte-welcome.png 	new file:   src/lib/images/svelte-welcome.webp 	new file:   src/routes/+page.js 	new file:   src/routes/+page.svelte 	new file:   src/routes/about/+page.js 	new file:   src/routes/about/+page.svelte 	new file:   src/routes/new/+page.js 	new file:   src/routes/new/+page.svelte 	new file:   src/routes/styles.css 	new file:   src/routes/sverdle/+page.server.js 	new file:   src/routes/sverdle/+page.svelte 	new file:   src/routes/sverdle/how-to-play/+page.js 	new file:   src/routes/sverdle/how-to-play/+page.svelte 	new file:   src/routes/sverdle/words.server.js 	new file:   static/favicon.png 	new file:   static/robots.txt 	new file:   svelte.config.js 	new file:   tests/test.js 	new file:   vite.config.js

---
+++ src/routes/+page.svelte
@@ -0,0 +1,86 @@
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+<meta charset="utf-8">
+
+<script>
+	 import { Button } from 'sveltestrap';
+</script>
+
+<head>
+ <style type ='text/css'>
+ body{margin: 0}
+
+ </style>
+ </head>
+
+<body>
+	<h3> <strong>Введите параметры системы</strong></h3><br>
+	<div class="mb-3 row">
+		<label for="get_N" class="col-sm-1 col-form-label">
+			<strong>N</strong></label>
+			<div class="col-sm-4">
+			<input type="text" class="form-control" placeholder="Введите число заявок" id="get_N"/>
+			</div>
+</div><br>
+
+<div class="mb-3 row">
+	<label for="get_alfa" class="col-sm-1 col-form-label">
+		<strong>alfa</strong></label>
+		<div class="col-sm-4">
+		<input type="text" class="form-control" placeholder="Введите минимальное время генерации заявки" id="get_alfa"/>
+		</div>
+</div><br>
+
+<div class="mb-3 row">
+	<label for="get_beta" class="col-sm-1 col-form-label">
+		<strong>beta</strong></label>
+		<div class="col-sm-4">
+		<input type="text" class="form-control" placeholder="Введите максимальное время генерации заявки" id="get_beta"/>
+		</div>
+</div><br>
+
+<div class="mb-3 row">
+	<label for="get_source_amount" class="col-sm-1 col-form-label">
+		<strong>число источников</strong></label>
+		<div class="col-sm-4">
+		<input type="text" class="form-control" placeholder="Введите число источников" id="get_source_amount"/>
+		</div>
+</div><br>
+
+<div class="mb-3 row">
+	<label for="get_reciever_amount" class="col-sm-1 col-form-label">
+		<strong>число приёмников</strong></label>
+		<div class="col-sm-4">
+		<input type="text" class="form-control" placeholder="Введите число приёмников" id="get_reciever_amount"/>
+		</div>
+</div><br>
+
+<div class="mb-3 row">
+	<label for="get_buffer_size" class="col-sm-1 col-form-label">
+		<strong>размер буфера</strong></label>
+		<div class="col-sm-4">
+		<input type="text" class="form-control" placeholder="Введите размер буфера" id="get_buffer_size"/>
+		</div>
+</div><br>
+
+
+<div class="d-grid gap-5 d-md-flex">
+    <Button color=primary on:click>Установить параметры</Button>
+
+    <Button color=primary on:click>Обнулить параметры</Button>
+</div>
+<br>
+<br>
+<br>
+
+<h3> <strong>Выберите режим работы</strong></h3><br>
+
+<div class="d-grid gap-5 d-md-flex">
+        <Button color=primary on:click>Пошаговый режим</Button>
+
+        <Button color=primary on:click>Автоматический режим</Button><br>
+    </div>
+    <br>
+
+
+
+</body>
