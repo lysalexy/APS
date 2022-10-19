d9930d7cc35eda19a1092838922bfdece9b82a45
LysenkoAleksandra <orange260302@gmail.com>
2022-10-19 22:37:54 +0300

    Add generation of buffer, sources,receivers
    Changes to be committed:
    	modified:   src/routes/+page.svelte
        new file:   src/routes/store.js

---
+++ src/routes/store.js
@@ -0,0 +1,10 @@
+import { writable } from 'svelte/store';
+
+export const currentTime = writable(0);
+export const finishedRequests = writable(0);
+export const previousN = writable(0);
+export const previousP = writable(0);
+export const currentN = writable(0);
+export const sources = writable([]);
+export const recievers = writable([]);
+export const buffer = writable([]);
