38caf835861bf8b13ea102f91dc99cfc84fe7c19
LysenkoAleksandra <orange260302@gmail.com>
2022-10-24 11:36:56 +0300

    .

--- src/routes/OneStep.svelte
+++ src/routes/OneStep.svelte
@@ -1,9 +1,9 @@
 <script lang="ts">
 	import { buffer, sources, recievers, summaryAmountOfRequests,finishedRequests, alfa,beta,lambda, currentTime, currentEvent,generatedRequests,requestSource,requestNumber} from './store.js';
 	import { Button, Row, Col, Table} from 'sveltestrap';
-    import {Source} from './Source';
-    import {Receiver} from './Receiver';
-    import {Buffer, BufferElem} from './Buffer';
+    // import {Source} from './Source';
+    //import {Receiver} from './Receiver';
+    // import {Buffer, BufferElem} from './Buffer';
 
     let sourceColumns = ["Номер", "Время генерации следующей заявки", "Всего сгенерировано заявок", "Число отказов"];
     let currentSources =[""];
@@ -14,50 +14,8 @@
     let bufferColumns = ["Позиция", "Время постановки заявки", "Источник заявки", "Номер заявки в источнике"];
     let currentBuffer=[""];
 
-
-	export function doStep() {
-        if ($finishedRequests>$summaryAmountOfRequests)
-        {
-            let buf_size = $buffer.length;
-            buffer.set(new Buffer(buf_size));
-            
-            let source_amount = $sources.length;
-            let sor = [];
-            
-            for (let index = 0; index < source_amount; index++) {
-                let source = new Source(index+1);
-                source.generateNewRequestTime($alfa,$beta);
-                sor.push(source);
-            }
-            
-            sources.set(sor);
-            
-            let reciever_amount = $recievers.length;
-            let rec = [];
-            
-            for (let index = 0; index < reciever_amount; index++) {
-                 let receiver = new Receiver(index+1);
-                  receiver.generateFreeTime($lambda)
-                  rec.push(receiver);
-                }
-                
-                recievers.set(rec);
-        }       
-        else{
-            ///сравниваем занятые источники и занятые приёмники
-            //они предварительно уже отсортированы по времени
-            let busyReceivers=[];
-
-            for (let index = 0; index < $recievers.length; index++) {
-                    if ($recievers.at(index).getStatus()=='busy'){
-                      busyReceivers.push($recievers.at(index));
-                    }
-             }
-
-            if (Number(busyReceivers)==Number(0))//если нет занятых источников
-            {
-                //ближайшее событие-генерация новой заявки
-                currentTime.set($sources.at(0).getGenTime());///устанавливаем текущее время
+    function generateRequest(busyReceivers){
+        currentTime.set($sources.at(0).getGenTime());///устанавливаем текущее время
                 $sources.at(0).generateNewRequest($alfa, $beta, $sources);
                     
                 let recieversSortedSByNumber = $recievers.slice().sort((a, b) => a.getNumber() - b.getNumber());////сортируем приборы по номеру
@@ -84,58 +42,59 @@
                     }
                     let sortedS = $sources.slice().sort((a, b) => a.genTime - b.genTime);
                     sources.set(sortedS);
+    }
+
+    function freeReciever(busyReceivers){
+        finishedRequests.update(count=>count+1);///увеличиваем число обработанных заявок
+                
+        currentTime.set(busyReceivers.at(0).getFreeTime());///текущее время-время освобождения приёмника
+
+        if($buffer.hasBusyElements()){///в буфере есть заявки
+            $buffer.getRequest($sources, $generatedRequests, $currentTime);///получаем заявку из буфера
+            busyReceivers.at(0).setRequest($requestSource, $requestNumber,  $lambda, $sources, $recievers,$currentTime);///ставим заявку на освободившийся прибор
+            let sortedR = $recievers.slice().sort((a, b) => a.freeTime - b.freeTime);
+            recievers.set(sortedR);
+        }
+        else{//в буфере нет заявок
+            busyReceivers.at(0).setFreeStatus();
+        }
+    }
+
+	export function doStep() {
+        let busyReceivers=[];
+        for (let index = 0; index < $recievers.length; index++) {
+            if ($recievers.at(index).getStatus()=='busy'){
+                 busyReceivers.push($recievers.at(index));
+                 }
+        }
+
+        if ($generatedRequests==$summaryAmountOfRequests)///заканчиваем генерацию заявок
+        {
+            ///поменять время генерации след заявки во всех источниках на -
+            if ($finishedRequests<$summaryAmountOfRequests)
+            {
+                freeReciever(busyReceivers);
+            }
+        }       
+        else{
+            if (Number(busyReceivers)==Number(0))//если нет занятых источников
+            {
+                //ближайшее событие-генерация новой заявки
+                generateRequest(busyReceivers);
 
             }
             else{//есть занятые приёмники
-            {
                 if ($sources.at(0).genTime < busyReceivers.at(0).freeTime)//ближайшее событие-генерация новой заявки
                 
                 {
-                    currentTime.set($sources.at(0).getGenTime());///устанавливаем текущее время
-                    $sources.at(0).generateNewRequest($alfa, $beta, $sources);
-                    
-                    let recieversSortedSByNumber = $recievers.slice().sort((a, b) => a.getNumber() - b.getNumber());////сортируем приборы по номеру
-                    
-                    if (busyReceivers.length==$recievers.length){///если нет свободных приборов, ставим заявку в буфер или получаем отказ в случае его наполненности
-                    $buffer.setRequestOrDoResuse($sources.at(0).getNumber(),$sources.at(0).getGeneratedRequestsAmount(),$currentTime,$sources);
-                    }
-                    else{///если есть свободные приборы, ставим заявку на прибоp
-                        let minimalFreeIndex =-1;
-                         let recieversSortedSByNumber = $recievers.slice().sort((a, b) => a.getNumber() - b.getNumber());////сортируем приборы по номеру
-                         for (let index = 0; index < recieversSortedSByNumber.length; index++) {
-                            if (recieversSortedSByNumber.at(index).getStatus()=='free'){///берем первый свободный
-                                minimalFreeIndex=index;
-                                break;
-                            }
-                        }
-                        requestSource.set($sources.at(0).getNumber());
-                        requestNumber.set($sources.at(0).getGeneratedRequestsAmount());
-                        recieversSortedSByNumber.at(minimalFreeIndex).setRequest($sources.at(0).getNumber(), $sources.at(0).getGeneratedRequestsAmount(),$lambda, $sources, $recievers,$currentTime);
-                        let sortedR = $recievers.slice().sort((a, b) => a.freeTime - b.freeTime);
-                        recievers.set(sortedR);
-
-                    }
-                    let sortedS = $sources.slice().sort((a, b) => a.genTime - b.genTime);
-                    sources.set(sortedS);
+                    generateRequest(busyReceivers);
                 }
                 else{//ближайшее событие-освобождение прибора
-                    finishedRequests.update(count=>count+1);///увеличиваем число обработанных заявок
-                
-                    currentTime.set(busyReceivers.at(0).getFreeTime());///текущее время-время освобождения приёмника
-
-                    if($buffer.hasBusyElements()){///в буфере есть заявки
-                        $buffer.getRequest($sources, $generatedRequests, $currentTime);///получаем заявку из буфера
-                        busyReceivers.at(0).setRequest($requestSource, $requestNumber,  $lambda, $sources, $recievers,$currentTime);///ставим заявку на освободившийся прибор
-                        let sortedR = $recievers.slice().sort((a, b) => a.freeTime - b.freeTime);
-                        recievers.set(sortedR);
-                    }
-                     else{//в буфере нет заявок
-                        busyReceivers.at(0).setFreeStatus();
-                     }
+                    freeReciever(busyReceivers);
                 }
-
-            }
+           }
         }
+
         currentSources=[];
         let sortedSByNumber = $sources.slice().sort((a, b) => a.getNumber() - b.getNumber());
         for (let index = 0; index < sortedSByNumber.length; index++) {
@@ -156,12 +115,12 @@
             console.log(currElem);
             currentBuffer.push([(index+1), currElem.getTimeOfPasting(), currElem.getSourceNumber(), currElem.getNumber()]);
         }
-		console.log($sources);
-		console.log($recievers);
-        console.log($buffer);
-
-		
-	}
+		// console.log($sources);
+		// console.log($recievers);
+        // console.log($buffer);
+        // console.log($generatedRequests);
+        // console.log($summaryAmountOfRequests);
+        // console.log($finishedRequests);
 }
 </script>
 
